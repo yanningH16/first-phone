db25bb973e078f78d78b5ebcc194d23d6fdfd9c1
ZhangShuKang <1456907177@qq.com>
2017-10-12 20:35:35 +0800

    更新node服务启动

--- prod.server.js
+++ prod.server.js
@@ -1,16 +1,3 @@
-// var express = require('express')
-// var config = require('./config/index')
-// var app = express()
-//   // 端口
-// app.use(express.static('./dist'))
-// var port = process.env.PORT || config.build.port
-// module.exports = app.listen(port, function(err) {
-//   if (err) {
-//     console.log(err)
-//     return
-//   }
-//   console.log('Listening at http://localhost:' + port + '\n')
-// })
 const http = require('http')
 const fs = require('fs')
 const httpPort = 80
